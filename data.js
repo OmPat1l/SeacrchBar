594eefbf643f9b758bf10010b99a0e708d4633ae
Om Patil <omajitsinhpatil@gmail.com>
2023-01-12 12:02:34 +0530

    scroll overflow

--- data.js
+++ data.js
@@ -1,45 +1,108 @@
 var data = [
     {
-        "title": "Codifica",
-        "description": "Codifica",
+        "title": "FLASH ",
+        "description": "AMUZIA",
         "link": "../EventList/WebScape/index.html",
         "isboolean": "true"
     },
     {
-        "title": "Codifica1",
-        "description": "Codifica1",
-        "link": "../EventList/WebScape/index.html",
-        "isboolean": "true"
-
-
-    },
-    {
-        "title": "Codifica3",
-        "description": "Codifica3",
+        "title": "FANDOM",
+        "description": "AMUZIA",
         "link": "../EventList/WebScape/index.html",
         "isboolean": "true"
 
 
     },
     {
-        "title": "Codifica4",
-        "description": "Codifica4",
+        "title": "TAKE OFF",
+        "description": "AVIONICA",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    },
+    {
+        "title": "CONTRAPTION",
+        "description": "AVIONICA",
         "link": "../EventList/WebScape/index.html",
         "isboolean": "false"
 
 
     },
     {
-        "title": "Codifica5",
-        "description": "Codifica5",
+        "title": "FLIP IT",
+        "description": "CODIFICA",
         "link": "../EventList/WebScape/index.html",
         "isboolean": "false"
 
 
     },
     {
-        "title": "Codifica6",
-        "description": "Codifica6",
+        "title": "CODE JUNKIE",
+        "description": "CODIFICA",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    }, {
+        "title": "WEBSCAPE",
+        "description": "CODIFICA",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    }, {
+        "title": "RETRACER",
+        "description": "CODIFICA",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    }, {
+        "title": "INDIKYA",
+        "description": "ILLUMINATI",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    }, {
+        "title": "TECH DE BAIT",
+        "description": "ILLUMINATI",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    }, {
+        "title": "TORQUEST",
+        "description": "ILLUMINATI",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    }, {
+        "title": "CODE JUNKIE",
+        "description": "CODIFICA",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    }, {
+        "title": "CODE JUNKIE",
+        "description": "CODIFICA",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    }, {
+        "title": "CODE JUNKIE",
+        "description": "CODIFICA",
+        "link": "../EventList/WebScape/index.html",
+        "isboolean": "true"
+
+
+    }, {
+        "title": "CODE JUNKIE",
+        "description": "CODIFICA",
         "link": "../EventList/WebScape/index.html",
         "isboolean": "true"
 
