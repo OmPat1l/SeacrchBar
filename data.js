d92978eee04c540f22d5519dbf440657359f839f
Om Patil <omajitsinhpatil@gmail.com>
2023-01-12 10:46:49 +0530

    first

---
+++ data.js
@@ -0,0 +1,36 @@
+var data = [
+    {
+        "title": "Codifica",
+        "description": "Codifica",
+        "link": ""
+    },
+    {
+        "title": "Codifica1",
+        "description": "Codifica1",
+        "link": ""
+
+    },
+    {
+        "title": "Codifica3",
+        "description": "Codifica3",
+        "link": ""
+
+    },
+    {
+        "title": "Codifica4",
+        "description": "Codifica4",
+        "link": ""
+
+    },
+    {
+        "title": "Codifica5",
+        "description": "Codifica5",
+        "link": ""
+
+    },
+    {
+        "title": "Codifica6",
+        "description": "Codifica6",
+        "link": ""
+
+    },]
